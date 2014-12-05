9d162e57c40b8bc622736564ca4f5813da70ce5d
Ancor Gonzalez Sosa <ancor@suse.de>
2014-12-05 15:18:17 +0100

    First mockup

--- src/lib/journalctl/mockup_dialog.rb
+++ src/lib/journalctl/mockup_dialog.rb
@@ -19,26 +19,131 @@
 require "yast"
 
 Yast.import "UI"
+Yast.import "Label"
 
 module Journalctl
+  # Dialog to display journal entries with several filtering options
   class MockupDialog
 
     include Yast::UIShortcuts
     include Yast::I18n
     include Yast::Logger
 
+    def initialize
+      textdomain "journalctl"
+    end
+     
+    # Displays the dialog
     def run
+      return unless create_dialog
 
+      begin
+        return event_loop
+      ensure
+        close_dialog
+      end
+    end
+
+  private
+
+    # Draws the dialog
+    def create_dialog
       Yast::UI.OpenDialog(
         Opt(:decorated, :defaultsize),
-        Label(_("Hello world!"))
+        VBox(
+          # Header
+          Heading(_("Journal entries")),
+
+          # Boot selector
+          Frame(
+            _("Log entries for"),
+            boot_widget
+          ),
+          VSpacing(0.3),
+
+          # Filter checkboxes
+          Frame(
+            _("Filters"),
+            additional_filters_widget
+          ),
+          VSpacing(0.3),
+
+          # Refresh
+          Right(PushButton(Id(:refresh), _("Refresh"))),
+          VSpacing(0.3),
+
+          # Log entries
+          table,
+          VSpacing(0.3),
+
+          # Quit button
+          PushButton(Id(:cancel), Yast::Label.QuitButton)
+        )
       )
+    end
 
-      input = Yast::UI.UserInput
-
-      log.info "Received #{input}"
-
+    def close_dialog
       Yast::UI.CloseDialog
+    end
+
+    # Simple event loop 
+    def event_loop
+      loop do
+        input = Yast::UI.UserInput
+        if input == :cancel
+          # Break the loop
+          break
+        else
+          log.warn "Unexpected input #{input}"
+        end
+      end
+    end
+
+    # Table widget to display log entries
+    def table
+      Table(
+        Id(:entries_table),
+        Opt(:keepSorting),
+        Header(
+          _("Time"),
+          _("Source"),
+          _("Message"),
+        ),
+        []
+      )
+    end
+
+    # Widget allowing to select a boot option
+    def boot_widget
+      RadioButtonGroup(
+        Id(:boot),
+        VBox(
+          Left(RadioButton(Id(:boot_0), _("Current boot"))),
+          Left(RadioButton(Id(:boot_1), _("Previous boot")))
+        )
+      )
+    end
+
+    # Widget containing a checkbox per filter
+    def additional_filters_widget
+      filters = [
+        { name: :unit, label: _("For this systemd unit") },
+        { name: :file, label: _("For this file (executable or device)") },
+        { name: :priority, label: _("With at least this priority") }
+      ]
+
+      checkboxes = filters.map do |filter|
+        name = filter[:name]
+        Left(
+          HBox(
+            CheckBox(Id(name), filter[:label]),
+            HSpacing(1),
+            InputField(Id(:"#{name}_value"), "", "")
+          )
+        )
+      end
+
+      VBox(*checkboxes)
     end
   end
 end
